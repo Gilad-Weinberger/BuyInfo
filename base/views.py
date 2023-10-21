eb5d5f83855106d648371ecd8e987ee5909abf9d
Gilad Weinberger <giladwe21@gmail.com>
2023-10-21 23:05:49 +0300

    finish sidebar 1

--- base/views.py
+++ base/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
 def home(request):
-    context = {}
+    sidebar_menu = [
+    {'text': 'בית', 'link': '/', 'icon': 'fa-regular fa-house', 'alerts_count': 0},
+    {'text': 'קבלות', 'link': '/', 'icon': 'fa-regular fa-envelope', 'alerts_count': 0},
+    {'text': 'תזכורת', 'link': '/', 'icon': 'fa-regular fa-bell', 'alerts_count': 0},
+    {'text': 'ניתוח מידע', 'link': '/', 'icon': 'fa-regular fa-chart-mixed', 'alerts_count': 0},
+    {'text': 'הגדרות', 'link': '/', 'icon': 'fa-regular fa-gear', 'alerts_count': 0},
+    {'text': 'חשבונות', 'link': '/', 'icon': 'fa-regular fa-circle-user', 'alerts_count': 0},
+    {'text': 'עזרה', 'link': '/', 'icon': 'fa-regular fa-circle-info', 'alerts_count': 0},
+    ]
+    context = {"sidebar_menu": sidebar_menu}
 
     return render(request, 'base/home.html', context)
