7430fa20bb089b16ad63b3b97f8d8bb88ead3db5
Gilad Weinberger <giladwe21@gmail.com>
2023-10-22 15:20:51 +0300

    create user model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+import os
 
-# Create your models here.
+def profile_image_upload_path(instance, filename):
+    filename, ext = os.path.splitext(filename)
+    new_filename = f"profile_{instance.email}{ext}"
+    return os.path.join('profile_images', new_filename)
+
+class User(models.Model):
+    first_name = models.CharField(max_length=60, null=True)
+    last_name = models.CharField(max_length=60, null=True)
+    email = models.EmailField(unique=True, null=True)
+    avatar = models.ImageField(
+        null=True,
+        blank=True,
+        upload_to=profile_image_upload_path
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
