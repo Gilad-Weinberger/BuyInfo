70c1ca9e28522db4f2f97c7f03687dfed38e32bf
Gilad Weinberger <giladwe21@gmail.com>
2023-10-24 01:04:48 +0300

    add dict of expenses_distribution to base views

--- base/views.py
+++ base/views.py
@@ -1,20 +1,30 @@
 from django.shortcuts import render
+from collections import defaultdict
+from django.contrib.auth.models import User
+from .models import Expense
 
 def home(request):
     sidebar_menu = [
-    {'text': 'בית', 'link': '/', 'icon': 'fa-regular fa-house', 'alerts_count': 0},
-    {'text': 'קבלות', 'link': '/', 'icon': 'fa-regular fa-envelope', 'alerts_count': 0},
-    {'text': 'תזכורת', 'link': '/', 'icon': 'fa-regular fa-bell', 'alerts_count': 0},
-    {'text': 'ניתוח מידע', 'link': '/', 'icon': 'fa-regular fa-chart-mixed', 'alerts_count': 0},
-    {'text': 'הגדרות', 'link': '/', 'icon': 'fa-regular fa-gear', 'alerts_count': 0},
-    {'text': 'חשבונות', 'link': '/', 'icon': 'fa-regular fa-circle-user', 'alerts_count': 0},
-    {'text': 'עזרה', 'link': '/', 'icon': 'fa-regular fa-circle-info', 'alerts_count': 0},
+        {'text': 'בית', 'link': '/', 'icon': 'fa-regular fa-house', 'alerts_count': 0},
+        {'text': 'קבלות', 'link': '/', 'icon': 'fa-regular fa-envelope', 'alerts_count': 0},
+        {'text': 'תזכורת', 'link': '/', 'icon': 'fa-regular fa-bell', 'alerts_count': 0},
+        {'text': 'ניתוח מידע', 'link': '/', 'icon': 'fa-regular fa-chart-mixed', 'alerts_count': 0},
+        {'text': 'הגדרות', 'link': '/', 'icon': 'fa-regular fa-gear', 'alerts_count': 0},
+        {'text': 'חשבונות', 'link': '/', 'icon': 'fa-regular fa-circle-user', 'alerts_count': 0},
+        {'text': 'עזרה', 'link': '/', 'icon': 'fa-regular fa-circle-info', 'alerts_count': 0},
     ]
 
     user = None
+    expenses_distribution = None
     if request.user.is_authenticated:
         user = request.user
 
-    context = {"sidebar_menu": sidebar_menu, "user": user}
+        user_expenses = Expense.objects.filter(user=user)
+        expenses_distribution = defaultdict(lambda: defaultdict(lambda: defaultdict(int)))
+        for expense in user_expenses:
+            year, month, day = expense.date.year, expense.date.month, expense.date.day
+            expenses_distribution[year][month][day] += expense.price
+
+    context = {"sidebar_menu": sidebar_menu, "user": user, "expenses_distribution": expenses_distribution}
 
     return render(request, 'base/home.html', context)
