b2a95eb21fd05b41a08b6927816f708a46d9531c
Gilad Weinberger <giladwe21@gmail.com>
2023-10-30 13:00:31 +0200

    remove if user.is_authenticated base views home

--- base/views.py
+++ base/views.py
@@ -21,54 +21,46 @@ def home(request):
         {'text': 'עזרה', 'link': '/', 'icon': 'fa-regular fa-circle-info', 'alerts_count': 0},
     ]
 
-    user = None
-    expenses_distribution = None
-    user_family = None
-    popular_topics_list = None
-    recent_receipt = None
-    activities_track = None
-    all_user_receipts = None
-    if request.user.is_authenticated:
-        user = request.user
-        user_family = Family.objects.filter(
-            Q(parents=user) | Q(kids=user)
-        ).first()
+    user = request.user
+    user_family = Family.objects.filter(
+        Q(parents=user) | Q(kids=user)
+    ).first()
 
-        user_expenses = Expense.objects.filter(user=user)
-        expenses_distribution = {}
-        activities_track = {}
-        for expense in user_expenses:
-            year = expense.date.year
-            month = expense.date.month
-            day = expense.date.day
-            if year not in expenses_distribution:
-                expenses_distribution[year] = {}
-                activities_track[year] = {}
-            if month not in expenses_distribution[year]:
-                expenses_distribution[year][month] = {}
-                activities_track[year][month] = {}
-            if day not in expenses_distribution[year][month]:
-                expenses_distribution[year][month][day] = int(expense.price)
-                activities_track[year][month][day] = 1
-            else:
-                expenses_distribution[year][month][day] += int(expense.price)
-                activities_track[year][month][day] += 1
-        
-        current_month = timezone.now().month  
-        current_month_expenses = user_expenses.filter(date__month=current_month)
-        popular_topics = current_month_expenses.values('type__name').annotate(total_spent=Sum('price')).order_by('-total_spent')[:3]
-        popular_topics_list = [
-            {
-            'topic': topic['type__name'],  
-            'amountSpent': topic['total_spent']  
-            } for topic in popular_topics
-        ]
+    user_expenses = Expense.objects.filter(user=user)
+    expenses_distribution = {}
+    activities_track = {}
+    for expense in user_expenses:
+        year = expense.date.year
+        month = expense.date.month
+        day = expense.date.day
+        if year not in expenses_distribution:
+            expenses_distribution[year] = {}
+            activities_track[year] = {}
+        if month not in expenses_distribution[year]:
+            expenses_distribution[year][month] = {}
+            activities_track[year][month] = {}
+        if day not in expenses_distribution[year][month]:
+            expenses_distribution[year][month][day] = int(expense.price)
+            activities_track[year][month][day] = 1
+        else:
+            expenses_distribution[year][month][day] += int(expense.price)
+            activities_track[year][month][day] += 1
+    
+    current_month = timezone.now().month  
+    current_month_expenses = user_expenses.filter(date__month=current_month)
+    popular_topics = current_month_expenses.values('type__name').annotate(total_spent=Sum('price')).order_by('-total_spent')[:3]
+    popular_topics_list = [
+        {
+        'topic': topic['type__name'],  
+        'amountSpent': topic['total_spent']  
+        } for topic in popular_topics
+    ]
 
-        for receipt in user.all_user_receipts:
-            receipt.reduce_receipt_products()
-        all_user_receipts = user.all_user_receipts
+    for receipt in user.all_user_receipts:
+        receipt.reduce_receipt_products()
+    all_user_receipts = user.all_user_receipts
 
-        recent_receipt = Receipt.objects.filter(user=user).order_by('-date').first()
+    recent_receipt = Receipt.objects.filter(user=user).order_by('-date').first()
 
     if request.method == 'POST':
         product_id = request.POST.get('product_id')
